d9155390513982c1ae8ee7cb4ffc1633a92f1a72
NathanM0153 <99769114+NathanM0153@users.noreply.github.com>
2023-02-27 16:27:00 -0600

    f

--- HLTBSteam.py
+++ HLTBSteam.py
@@ -13,19 +13,27 @@ def getGames(): #returns list of steam games from excel file
     rows = list(ws.rows)
     i = 0
     gameList = []
-    while i < 734:
+    while i < 734: #game count, make this dynamic later
         for j in rows[i]:
             gameList.append(j.value)
         i+=1
     return gameList
     
-def findGame(name): #returns an integer for hour completion
-    gameList = getGames()
-    results = HowLongToBeat().search(name)
-    if results_list is not None and len(results_list) > 0:
-        best_element = max(results_list, key=lambda element: element.similarity)
-        
+def findGame(name): #returns a JSON result to be interpreted
+    results = HowLongToBeat().search(name, similarity_case_sensitive=False)
+    if results is not None and len(results) > 0:
+        best_element = max(results, key=lambda element: element.similarity)
+        return best_element
+    else:
+        print("Not found")
+        return
 
+def parse():
+    game = "Darkest Dungeon"
+    json = findGame(game)
+    result = __parse_web_result(game,json,None,False)
+    return result
+    
 
 
 
