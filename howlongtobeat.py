10d7489d13bf98df55b267c8f577fd8dcefae10e
NathanM0153 <99769114+NathanM0153@users.noreply.github.com>
2023-02-27 14:36:01 -0600

    excel file

--- howlongtobeat.py
+++ howlongtobeat.py
@@ -1,24 +1,81 @@
+#modules needed: aiohttp, requests, fake_useragent, openpyxl
+#scrape games via ParseHub
+
 from howlongtobeatpy import HowLongToBeat #https://pypi.org/project/howlongtobeatpy/
 from openpyxl import load_workbook #https://ehmatthes.github.io/pcc_2e/beyond_pcc/extracting_from_excel/
 
 #class howlongtobeat():
 file = "C:/Users/Nathan/Documents/PySteamTimer/SteamGames.xlsx"
 wb = load_workbook(file)
-ws = wb["Sheet1"]
+ws = wb["run_results"]
 
-def accessSheet(): 
+def getGames(): #returns list of steam games from excel file
     rows = list(ws.rows)
     i = 0
-    while i < 5:
+    gameList = []
+    while i < 734:
         for j in rows[i]:
-            print(j.value, end = " ")
-        print("") #new line
+            gameList.append(j.value)
         i+=1
-
+    return gameList
     
-def findGame(gameList):
+def findGame(name): #returns an integer for hour completion
+    gameList = getGames()
+    results = HowLongToBeat().search(name)
+    if results_list is not None and len(results_list) > 0:
+        best_element = max(results_list, key=lambda element: element.similarity)
+        
 
-    for i in gameList:
-        results += HowLongToBeat().search(gameList[i])
-        #gamestring = join([str(item) for item in results])
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def scratch():
+    rows = list(ws.rows)
+    i = 0
+    j = 0
+    gameList = []
+    while True:
+        for j in rows[i]:
+            if i.value == "":
+                return gameList
+            gameList.append(i.value)
+        i+=1
+    return gameList
+
+
+    rows = list(ws.rows)
+    i = 0
+    gameList = []
+    while i < 734:
+        for j in rows[i]:
+            gameList.append(j.value)
+        i+=1
+    return gameList
