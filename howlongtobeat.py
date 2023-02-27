43638e8c9e0c85b7000ff202d10aa761bdab74de
NathanM0153 <99769114+NathanM0153@users.noreply.github.com>
2023-02-27 10:22:39 -0600

    fixed aiohttp

--- howlongtobeat.py
+++ howlongtobeat.py
@@ -1,8 +1,24 @@
-from howlongtobeatpy import HowLongToBeat
+from howlongtobeatpy import HowLongToBeat #https://pypi.org/project/howlongtobeatpy/
+from openpyxl import load_workbook #https://ehmatthes.github.io/pcc_2e/beyond_pcc/extracting_from_excel/
 
-class howlongtobeat():
-    def findGame(gameList):
+#class howlongtobeat():
+file = "C:/Users/Nathan/Documents/PySteamTimer/SteamGames.xlsx"
+wb = load_workbook(file)
+ws = wb["Sheet1"]
 
-        for i in gameList:
-            results = HowLongToBeat().search(gameList[i])
-            #gamestring = join([str(item) for item in results])
+def accessSheet(): 
+    rows = list(ws.rows)
+    i = 0
+    while i < 5:
+        for j in rows[i]:
+            print(j.value, end = " ")
+        print("") #new line
+        i+=1
+
+    
+def findGame(gameList):
+
+    for i in gameList:
+        results += HowLongToBeat().search(gameList[i])
+        #gamestring = join([str(item) for item in results])
+
